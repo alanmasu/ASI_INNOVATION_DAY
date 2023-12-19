9b003ae5be295cbe11a56b42c06b10fa88b6639f
alanmasu <alan.masutti@gmail.com>
2023-12-19 14:43:27 +0100

    Inserito controllo H bridge

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,21 @@
 #include <Arduino.h>
 
+#define H_BRIDGE_IN1 7
+#define H_BRIDGE_IN2 8
+#define H_BRIDGE_PWM 9
+
 // put function declarations here:
 void setup() {
-
+    Serial.begin(9600);
+    Serial.println("Hello ASI!");
+    pinMode(H_BRIDGE_IN1, OUTPUT);
+    pinMode(H_BRIDGE_IN2, OUTPUT);
+    pinMode(H_BRIDGE_PWM, OUTPUT);
+    analogWrite(H_BRIDGE_PWM, 255);
+    digitalWrite(H_BRIDGE_IN1, HIGH);
+    digitalWrite(H_BRIDGE_IN2, LOW);
 }
 
 void loop() {
-  
+
 }
